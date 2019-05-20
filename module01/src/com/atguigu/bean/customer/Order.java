60f1d9db1142eaaf85646610e67bd36fd50d1876
daniellizs <seliman_li@sina.com>
2019-05-20 22:43:32 +0800

    测试类

---
+++ module01/src/com/atguigu/bean/customer/Order.java
@@ -0,0 +1,40 @@
+package com.atguigu.bean.customer;
+
+public class Order {
+    private String id;
+    private String userName;
+    private String idNo;
+    private String orderNo;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getIdNo() {
+        return idNo;
+    }
+
+    public void setIdNo(String idNo) {
+        this.idNo = idNo;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+}
